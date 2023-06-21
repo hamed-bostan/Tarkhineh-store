1819d295692bb252952e5fdb9874837433618c9c
hamed <hamed.bostan2@gmail.com>
2023-06-21 13:34:42 +0330

    data is made

---
+++ src/components/home/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Home.scss";
+
+const Home = () => {
+  return <div>home page</div>;
+};
+
+export default Home;
---
+++ src/components/home/navbar/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return <div>navbar</div>;
+};
+
+export default Navbar;
