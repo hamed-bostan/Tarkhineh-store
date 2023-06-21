34299664ebe2931ecb6678714e52e0879ebbbbf0
hamed <hamed.bostan2@gmail.com>
2023-06-22 02:21:37 +0330

    navbar with padding aproch has added

--- src/components/home/navbar/Navbar.js
+++ src/components/home/navbar/Navbar.js
@@ -7,57 +7,43 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="navbar_container">
-      <div className="navbar_wrapper">
-        {/* Header section */}
-        <div className="navbar_header">
-          <div className="navbar_right_side_container">
-            <img
-              onClick={() => setIsOpen(!isOpen)}
-              className="navbar_icon"
-              src="assets/images/icons/menu.png"
-              alt=""
-            />
-          </div>
-          <div className="navbar_center_container">
-            <img
-              className="navbar_logo"
-              src="assets/images/icons/Logo.png"
-              alt=""
-            />
-          </div>
-          <div className="navbar_left_side_container">
-            <img
-              className="navbar_icon"
-              src="assets/images/icons/shopping-cart.png"
-              alt=""
-            />
-            <img
-              className="navbar_icon"
-              src="assets/images/icons/user.png"
-              alt=""
-            />
-          </div>
-        </div>
-        {/* End of Header section */}
-        {/* Link section*/}
-        <div className="navbar_links_container">
-          {navbarItems.map((link) => {
-            return (
-              <div
-                className={`${
-                  isOpen
-                    ? "navbar_links_wrapper_show"
-                    : "navbar_links_wrapper_not_show"
-                }`}
-              >
-                <p className="navbar_link_item">{link.text}</p>
-              </div>
-            );
-          })}
-        </div>
-        {/* End of Link section */}
+    <div className="navbar_header">
+      {/* Header section */}
+      <div className="navbar_right_side_container">
+        <img
+          onClick={() => setIsOpen(!isOpen)}
+          className="navbar_icon"
+          src="assets/images/icons/menu.png"
+          alt=""
+        />
       </div>
+      <div className="navbar_center_container">
+        <img
+          className="navbar_logo"
+          src="assets/images/icons/Logo.png"
+          alt=""
+        />
+      </div>
+      <div className="navbar_left_side_container">
+        <img
+          className="navbar_icon"
+          src="assets/images/icons/shopping-cart.png"
+          alt=""
+        />
+        <img
+          className="navbar_icon"
+          src="assets/images/icons/user.png"
+          alt=""
+        />
+      </div>
+      {/* End of Header section */}
+      {/* Link section*/}
+      <div className="navbar_links_container">
+        {navbarItems.map((link) => {
+          return <p className="navbar_link_item">{link.text}</p>;
+        })}
+      </div>
+      {/* End of Link section */}
     </div>
   );
 };
