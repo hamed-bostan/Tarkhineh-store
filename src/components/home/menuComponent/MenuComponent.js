ee07b0232ae61d230e61b0a7299ec44085556120
hamed <hamed.bostan2@gmail.com>
2023-06-24 03:21:01 +0330

    working on font-size of menu component button

--- src/components/home/menuComponent/MenuComponent.js
+++ src/components/home/menuComponent/MenuComponent.js
@@ -1,8 +1,26 @@
 import React from "react";
 import "./MenuComponent.scss";
+import { menuInformation } from "../../../data";
 
 const MenuComponent = () => {
-  return <div>MenuComponent</div>;
+  return (
+    <>
+      <p className="menu_component_title">منوی رستوران</p>
+      <div className="menu_component_wrapper">
+        {menuInformation.map((item) => {
+          return (
+            <div className="menu_component_item_container">
+              <div className="menu_component_image_container">
+                <img src={item.image} alt="" className="menu_component_image" />
+                <div className="menu_component_background"></div>
+                <span className="menu_component_text">{item.text}</span>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default MenuComponent;
