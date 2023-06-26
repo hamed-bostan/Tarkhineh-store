d5a9b10878c27d1a0cdafe00f27128873f36812b
hamed <hamed.bostan2@gmail.com>
2023-06-26 18:07:51 +0330

    footer component for desktop ui has finished

--- src/components/home/footer/Footer.js
+++ src/components/home/footer/Footer.js
@@ -4,15 +4,28 @@ import "./Footer.scss";
 const Footer = () => {
   return (
     <div className="footer_container">
+      {/* sidebar section */}
       <div className="footer_sidebar">
         <span className="footer_title">دسترسی آسان</span>
         <span className="footer_text">پرسش‌های متداول</span>
         <span className="footer_text">قوانین ترخینه</span>
         <span className="footer_text">حریم خصوصی</span>
         <div className="footer_icon_container">
-          <img src="assets/images/icons/twitter.png" alt=""  className="footer_icon"/>
-          <img src="assets/images/icons/instagram.png" alt="" className="footer_icon"/>
-          <img src="assets/images/icons/telegram.png" alt="" className="footer_icon"/>
+          <img
+            src="assets/images/icons/twitter.png"
+            alt=""
+            className="footer_icon"
+          />
+          <img
+            src="assets/images/icons/instagram.png"
+            alt=""
+            className="footer_icon"
+          />
+          <img
+            src="assets/images/icons/telegram.png"
+            alt=""
+            className="footer_icon"
+          />
         </div>
       </div>
       <div className="footer_sidebar">
@@ -21,6 +34,42 @@ const Footer = () => {
         <span className="footer_text">شعبه چالوس</span>
         <span className="footer_text">شعبه اقدسیه</span>
         <span className="footer_text">شعبه ونک</span>
+      </div>
+      {/* End of sidebar section */}
+
+      {/* input section */}
+      <div className="footer_input_container">
+        <span className="footer_title">پیام به ترخینه </span>
+        <div className="footer_input_main_wrapper">
+          <div className="footer_input_wrapper">
+            <input
+              type="text"
+              placeholder="نام و نام خانوادگی"
+              className="footer_input_item"
+            />
+            <input
+              type="text"
+              placeholder="شماره تماس"
+              className="footer_input_item"
+            />
+            <input
+              type="email"
+              placeholder="آدرس ایمیل (اختیاری)"
+              className="footer_input_item"
+            />
+          </div>
+          <div className="footer_your_message_container">
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+              placeholder="پیام شما"
+              className="footer_your_message_item"
+            ></textarea>
+          </div>
+        </div>
+        <button className="footer_button">ارسال پیام</button>
       </div>
     </div>
   );
