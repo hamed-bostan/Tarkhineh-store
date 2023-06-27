4582296a3e4cb93b8ca7e4c8d13db4280062b3ff
hamed <hamed.bostan2@gmail.com>
2023-06-27 22:04:12 +0330

    menu page has refactored

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Menu.scss";
+import { useState } from "react";
+import { foodInformation } from "../../data";
 // Components
 import Navbar from "../home/navbar/Navbar";
 import Header from "../home/header/Header";
-import { foodInformation } from "../../data";
-import { useState } from "react";
 import Category from "./category/Category";
 import Search from "./search/Search";
+import MenuCard from "./menuCard/MenuCard";
 
 const Menu = () => {
   const [data, setData] = useState(foodInformation);
@@ -35,51 +36,9 @@ const Menu = () => {
       </div>
       {/* End of title section */}
       {/* card section */}
-      <section className="menu_items_main_container">
-        {data.map((item) => {
-          const {
-            image,
-            title,
-            description,
-            highPrice,
-            discount,
-            discountedPrice,
-            star,
-          } = item;
-          return (
-            <article className="menu_items_container">
-              <img src={image} alt="" className="menu_image" />
-              <div className="menu_items_information_container">
-                {/* first div */}
-                <div className="menu_single_item_container">
-                  <span className="menu_single_item_title">{title}</span>
-                  <div className="menu_high_price_container">
-                    <span className="menu_high_price">{highPrice}</span>
-                    <span className="menu_discount_item">{discount}</span>
-                  </div>
-                </div>
-                {/* next div */}
-                <div className="menu_single_item_container">
-                  <p>{description.slice(0, 20)} ...</p>
-                  <span>{discountedPrice}</span>
-                </div>
-                {/* next div */}
-                <div className="menu_single_item_container">
-                  <img
-                    src="assets/images/icons/heart.png"
-                    alt=""
-                    className="menu_heart_icon"
-                  />
-                  <span>{star}</span>
-                  <button className="menu_add_to_cart_button">
-                    افزودن به سبد خرید
-                  </button>
-                </div>
-              </div>
-            </article>
-          );
-        })}
-      </section>
+      {data.map((item) => {
+        return <MenuCard data={item} key={item.id} />;
+      })}
       {/* End of card section */}
     </div>
   );
--- src/components/menu/menuCard/MenuCard.js
+++ src/components/menu/menuCard/MenuCard.js
@@ -1,52 +1,57 @@
 import React from "react";
 import "./MenuCard.scss";
+// Redux
+import { useDispatch } from "react-redux";
+import { addItem } from "../../redux/cart/cartAction";
 
-const MenuCard = ({data}) => {
+const MenuCard = ({ data }) => {
+  const dispatch = useDispatch();
+
+  const {
+    image,
+    title,
+    description,
+    highPrice,
+    discount,
+    discountedPrice,
+    star,
+  } = data;
+
   return (
     <section className="menu_items_main_container">
-      {data.map((item) => {
-        const {
-          image,
-          title,
-          description,
-          highPrice,
-          discount,
-          discountedPrice,
-          star,
-        } = item;
-        return (
-          <article className="menu_items_container">
-            <img src={image} alt="" className="menu_image" />
-            <div className="menu_items_information_container">
-              {/* first div */}
-              <div className="menu_single_item_container">
-                <span className="menu_single_item_title">{title}</span>
-                <div className="menu_high_price_container">
-                  <span className="menu_high_price">{highPrice}</span>
-                  <span className="menu_discount_item">{discount}</span>
-                </div>
-              </div>
-              {/* next div */}
-              <div className="menu_single_item_container">
-                <p>{description.slice(0, 20)} ...</p>
-                <span>{discountedPrice}</span>
-              </div>
-              {/* next div */}
-              <div className="menu_single_item_container">
-                <img
-                  src="assets/images/icons/heart.png"
-                  alt=""
-                  className="menu_heart_icon"
-                />
-                <span>{star}</span>
-                <button className="menu_add_to_cart_button">
-                  افزودن به سبد خرید
-                </button>
-              </div>
+      <article className="menu_items_container">
+        <img src={image} alt="" className="menu_image" />
+        <div className="menu_items_information_container">
+          {/* first div */}
+          <div className="menu_single_item_container">
+            <span className="menu_single_item_title">{title}</span>
+            <div className="menu_high_price_container">
+              <span className="menu_high_price">{highPrice}</span>
+              <span className="menu_discount_item">{discount}</span>
             </div>
-          </article>
-        );
-      })}
+          </div>
+          {/* next div */}
+          <div className="menu_single_item_container">
+            <p>{description.slice(0, 20)} ...</p>
+            <span>{discountedPrice}</span>
+          </div>
+          {/* next div */}
+          <div className="menu_single_item_container">
+            <img
+              src="assets/images/icons/heart.png"
+              alt=""
+              className="menu_heart_icon"
+            />
+            <span>{star}</span>
+            <button
+              className="menu_add_to_cart_button"
+              onClick={() => dispatch(addItem(data))}
+            >
+              افزودن به سبد خرید
+            </button>
+          </div>
+        </div>
+      </article>
     </section>
   );
 };
