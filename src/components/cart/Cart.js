12481744d7bc036ac712d444ae3e14a5a8239f2c
hamed <hamed.bostan2@gmail.com>
2023-06-28 15:17:05 +0330

    cart payment has refactored

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import Navbar from "../home/navbar/Navbar";
 import CartCard from "./cartCard/CartCard";
 import CartHeader from "./cartHeader/CartHeader";
+import CartPayment from "./cartPayment/CartPayment";
 
 const Cart = () => {
   const dispatch = useDispatch();
@@ -23,44 +24,7 @@ const Cart = () => {
           })}
         </div>
         {/* End of card section */}
-        <div className="cart_payment_container">
-          <hr className="cart_horizontal_line" />
-          <div className="cart_payment_discount">
-            <span>تخفیف محصولات</span>
-            <span className="cart_payment_price">۶۳٬۰۰۰ تومان</span>
-          </div>
-          <hr className="cart_horizontal_line" />
-          <div className="cart_payment_delivery">
-            <span>هزینه ارسال</span>
-            <span className="cart_payment_price">0 تومان</span>
-          </div>
-          <div className="cart_payment_warning">
-            <img
-              src="assets/images/icons/warning-2.png
-      "
-              alt=""
-              className="cart_payment_warning_icon"
-            />
-            <p className="cart_payment_warning_text">
-              هزینه ارسال در ادامه بر اساس آدرس، زمان و نحوه ارسال انتخابی شما
-              محاسبه و به این مبلغ اضافه خواهد شد.
-            </p>
-          </div>
-          <hr className="cart_horizontal_line" />
-          <div className="cart_payment_total">
-            <span>مبلغ قابل پرداخت</span>
-            <span className="cart_payment_total_price">۵۴۲٬۰۰۰ تومان</span>
-          </div>
-          <div className="cart_payment_logging_button_container">
-            <img src="assets/images/icons/user_02.png" alt="" 
-className="cart_payment_logging_user_icon"
-
-            />
-            <button className="cart_payment_logging_button">
-              ورود/ثبت‌نام
-            </button>
-          </div>
-        </div>
+        <CartPayment />
       </div>
       {/* End of information section */}
     </div>
---
+++ src/components/cart/cartPayment/CartPayment.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./CartPayment.scss";
+
+const CartPayment = () => {
+  return (
+    <div className="cart_payment_container">
+      <hr className="cart_horizontal_line" />
+      <div className="cart_payment_discount">
+        <span>تخفیف محصولات</span>
+        <span className="cart_payment_price">۶۳٬۰۰۰ تومان</span>
+      </div>
+      <hr className="cart_horizontal_line" />
+      <div className="cart_payment_delivery">
+        <span>هزینه ارسال</span>
+        <span className="cart_payment_price">0 تومان</span>
+      </div>
+      <div className="cart_payment_warning">
+        <img
+          src="assets/images/icons/warning-2.png"
+          alt=""
+          className="cart_payment_warning_icon"
+        />
+        <p className="cart_payment_warning_text">
+          هزینه ارسال در ادامه بر اساس آدرس، زمان و نحوه ارسال انتخابی شما
+          محاسبه و به این مبلغ اضافه خواهد شد.
+        </p>
+      </div>
+      <hr className="cart_horizontal_line" />
+      <div className="cart_payment_total">
+        <span>مبلغ قابل پرداخت</span>
+        <span className="cart_payment_total_price">۵۴۲٬۰۰۰ تومان</span>
+      </div>
+      <div className="cart_payment_logging_button_container">
+        <img
+          src="assets/images/icons/user_02.png"
+          alt=""
+          className="cart_payment_logging_user_icon"
+        />
+        <button className="cart_payment_logging_button">ورود/ثبت‌نام</button>
+      </div>
+    </div>
+  );
+};
+
+export default CartPayment;
