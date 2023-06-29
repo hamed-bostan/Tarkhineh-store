c8e584950a561755860185041273701ae6907858
hamed <hamed.bostan2@gmail.com>
2023-06-29 19:46:52 +0330

    working on cartCard component, for desktop ui with grid

--- src/components/cart/cartCard/CartCard.js
+++ src/components/cart/cartCard/CartCard.js
@@ -1,36 +1,67 @@
 import React from "react";
 import "./CartCard.scss";
 
-const CartCard = ({item}) => {
+const CartCard = ({ item }) => {
   return (
     <div className="cart_container">
       {/* information container */}
       <div className="cart_information_container">
-        <span className="cart_information_title">{item.title}</span>
-        <span className="cart_information_discounted_price">
-          {item.discountedPrice}
-        </span>
+        <p className="cart_information_title">{item.title}</p>
+        <img
+          src="assets/images/icons/trash.png"
+          alt=""
+          className="cart_information_trash_icon"
+        />
       </div>
       {/* End of information container */}
+
+      <div className="cart_final_price_container">
+        <p className="cart_final_price">{item.finalPrice}</p>
+      </div>
+
       {/* button container */}
       <div className="cart_button_container">
         <img
           src="assets/images/icons/increase_icon.png"
           alt=""
-          className="cart_information_button_icon_increase"
+          className="cart_increase_icon"
         />
         <img
           src="assets/images/icons/reduce_icon.png"
           alt=""
-          className="cart_information_button_icon_decrease"
+          className="cart_decrease_icon"
         />
         <img
           src="assets/images/icons/trash_green.png"
           alt=""
-          className="cart_information_button_icon_trash"
+          className="cart_trash_icon"
         />
       </div>
       {/* End of button container */}
+
+      {/* 1100px */}
+      {/* ************** */}
+      {/* 1100px */}
+
+      {/* <img src={item.image} alt="" 
+        className="cart_image"
+      /> */}
+
+      {/* description container */}
+      <div className="cart_description_container">
+        <p className="cart_description">{item.description.slice(0, 20)}</p>
+        <div className="cart_highPrice_discount_container">
+          <span className="cart_high_price">{item.highPrice}</span>
+          <span className="cart_discount">{item.discount}</span>
+        </div>
+      </div>
+      {/* End of description container */}
+
+      {/* star container */}
+      <div className="cart_star_container">
+        <span>{item.star}</span>
+      </div>
+      {/* End of star container */}
     </div>
   );
 };
