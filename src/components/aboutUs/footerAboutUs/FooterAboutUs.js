decc577b6f88e13d7aa235937b56901f59f324a3
hamed <hamed.bostan2@gmail.com>
2023-07-02 10:29:55 +0330

    about us page has finished

--- src/components/aboutUs/footerAboutUs/FooterAboutUs.js
+++ src/components/aboutUs/footerAboutUs/FooterAboutUs.js
@@ -8,31 +8,34 @@ const FooterAboutUs = () => {
         <img
           src="assets/images/icons/user_03.png"
           alt=""
-          className="footer_aboutUs_image"
+          className="footer_aboutUs_icon"
         />
         <span className="footer_aboutUs_text">پرسنلی مجرب و حرفه‌ای</span>
       </div>
+      <hr className="footer_aboutUs_vertical_line" />
       <div className="footer_aboutUs_wrapper">
         <img
           src="assets/images/icons/diagram_02.png"
           alt=""
-          className="footer_aboutUs_image"
+          className="footer_aboutUs_icon"
         />
         <span className="footer_aboutUs_text">کیفیت بالای غذاها</span>
       </div>
+      <hr className="footer_aboutUs_vertical_line" />
       <div className="footer_aboutUs_wrapper">
         <img
           src="assets/images/icons/home-wifi_02.png"
           alt=""
-          className="footer_aboutUs_image"
+          className="footer_aboutUs_icon"
         />
         <span className="footer_aboutUs_text">محیطی دلنشین و آرام</span>
       </div>
+      <hr className="footer_aboutUs_vertical_line" />
       <div className="footer_aboutUs_wrapper">
         <img
           src="assets/images/icons/menu-board_02.png"
           alt=""
-          className="footer_aboutUs_image"
+          className="footer_aboutUs_icon"
         />
         <span className="footer_aboutUs_text">منوی متنوع</span>
       </div>
