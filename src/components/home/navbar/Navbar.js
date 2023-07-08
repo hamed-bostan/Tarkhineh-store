e54f4a4a9a5419436afa7a3d2a6b5c1ef99c7c6d
hamed <hamed.bostan2@gmail.com>
2023-07-08 10:42:02 +0330

    refactoring the navbar

--- src/components/home/navbar/Navbar.js
+++ src/components/home/navbar/Navbar.js
@@ -9,109 +9,102 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const state = useSelector((state) => state.cartState);
 
-  //  set the buttons on active style
-  const [value, setValue] = useState(0);
-  const activeFunction = (index) => {
-    setValue(index);
-    console.log(index);
-  };
-  // End of set the buttons on active style
-
   return (
-    <div className="navbar_header">
-      <div className="navbar_toggle_container">
-        {!isOpen && (
-          <img
-            onClick={() => setIsOpen(true)}
-            className="navbar_icon menu_icon"
-            src="assets/images/icons/menu.png"
-            alt=""
-          />
-        )}
+    <div className="navbar_container">
+      <div className="navbar_header">
+        <div className="navbar_toggle_container">
+          {!isOpen && (
+            <img
+              onClick={() => setIsOpen(true)}
+              className="navbar_icon"
+              src="assets/images/icons/menu.png"
+              alt=""
+            />
+          )}
 
-        {isOpen && (
+          {isOpen && (
+            <img
+              onClick={() => setIsOpen(false)}
+              className="navbar_icon close_icon"
+              src="assets/images/icons/Close icon.png"
+              alt=""
+            />
+          )}
+        </div>
+        {/* logo section */}
+        <div className="navbar_logo_container">
           <img
-            onClick={() => setIsOpen(false)}
-            className="navbar_icon close_icon"
-            src="assets/images/icons/Close icon.png"
+            className="navbar_logo"
+            src="assets/images/icons/Logo.png"
             alt=""
           />
-        )}
-      </div>
-      <div className="navbar_logo_container">
-        <img
-          className="navbar_logo"
-          src="assets/images/icons/Logo.png"
-          alt=""
-        />
-      </div>
-      {/* Link section*/}
-      <div
-        className={`${
-          isOpen
-            ? "navbar_links_container navbar_links_show"
-            : "navbar_links_container"
-        }`}
-      >
-        {navbarItems.map((link) => {
-          return (
-            <div key={link.id}>
-              <NavLink
-                to={link?.path}
-                className={({ isActive }) =>
-                  isActive ? "navbar_link_active" : "navbar_link_not_active"
-                }
-              >
-                <div className="arrow_icon_container">
-                  <div className="navbar_mobile_icon_link_container">
-                    <img
-                      src={link.image}
-                      alt=""
-                      className="navbar_mobile_icon"
-                    />
-                    <p className="navbar_link_item">{link.text}</p>
-                  </div>
-                  {link?.icon && (
-                    <img src={link.icon} alt="" className="navbar_arrow_icon" />
-                  )}
-                </div>
-              </NavLink>
-              <hr className="navbar_horizontal_line" />
-            </div>
-          );
-        })}
-      </div>
-      {/* End of Link section */}
-      {/* Icons container */}
-      <div className="navbar_icons_container">
-        {navbarIcons.map((item, index) => {
-          return (
-            <div className="navbar_badge_container" key={item.id}>
-              <NavLink
-                to={item?.path}
-                className={({ isActive }) =>
-                  isActive ? "green_background_active" : "green_background"
-                }
-              >
-                <img className="navbar_icon" src={item.icons} alt="" />
-                {/* <span
-                  // className="navbar_icon"
-                  className={`${
-                    index === value ? "navbar_icon_active" : "navbar_icon"
-                  }`}
-                  onClick={() => activeFunction(index)}
+        </div>
+        {/* End of logo section */}
+        {/* Link section*/}
+        <div
+          className={`${
+            isOpen
+              ? "navbar_links_container navbar_links_show"
+              : "navbar_links_container"
+          }`}
+        >
+          {navbarItems.map((link) => {
+            return (
+              <div key={link.id}>
+                <NavLink
+                  to={link?.path}
+                  className={({ isActive }) =>
+                    isActive ? "navbar_link_active" : "navbar_link_not_active"
+                  }
                 >
-                  {item.icons}
-                </span> */}
-              </NavLink>
-              {item.path === "/cart" && (
-                <span className="navbar_badge">{state.itemsCounter}</span>
-              )}
-            </div>
-          );
-        })}
+                  <div className="navbar_arrow_icon_container">
+                    <div className="navbar_mobile_icon_link_container">
+                      <img
+                        src={link.image}
+                        alt=""
+                        className="navbar_mobile_icon"
+                      />
+                      <p>{link.text}</p>
+                    </div>
+                    {link?.icon && (
+                      <img
+                        src={link.icon}
+                        alt=""
+                        className="navbar_arrow_icon"
+                      />
+                    )}
+                  </div>
+                </NavLink>
+                <hr className="navbar_horizontal_line" />
+              </div>
+            );
+          })}
+        </div>
+        {/* End of Link section */}
+        {/* Icons container */}
+        <div className="navbar_icons_container">
+          {navbarIcons.map((item) => {
+            return (
+              <div className="navbar_badge_container" key={item.id}>
+                <NavLink
+                  to={item?.path}
+                  className={({ isActive }) =>
+                    isActive
+                      ? "navbar_green_background_active"
+                      : "navbar_green_background"
+                  }
+                >
+                  <img className="navbar_icon" src={item.icons} alt="" />
+                </NavLink>
+                {item.path === "/cart" && (
+                  <span className="navbar_badge">{state.itemsCounter}</span>
+                )}
+              </div>
+            );
+          })}
+        </div>
+        {/* End of Icons container */}
       </div>
-      {/* End of Icons container */}
     </div>
   );
 };
---
+++ src/components/home/navbar/navbarToggleComponent/NavbarToggleComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./NavbarToggleComponent.scss"
+
+const NavbarToggleComponent = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default NavbarToggleComponent;
